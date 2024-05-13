79e7997a8746cba6e6565e53b7c1efb870d95244
Eduardo Andreoli <53129882+C3Andreoli@users.noreply.github.com>
2024-05-13 20:56:38 -0300

    RISADAS E RISADAS
    Super foda

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -39,6 +39,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
@@ -49,13 +51,14 @@ TIM_HandleTypeDef htim4;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM4_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-uint16_t counter = 32;
+uint16_t read;
 /* USER CODE END 0 */
 
 /**
@@ -87,6 +90,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM4_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim4);
   /* USER CODE END 2 */
@@ -96,17 +100,16 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-    HAL_GPIO_TogglePin(GPIOC,KIT_LED_Pin);
-    HAL_Delay(200);
-
-    if(button_release(GPIOB,BTN1_Pin,0) & TIM4->ARR < (30*1030)){
-      htim4.Instance->ARR += 1030;
-  
-    }
-    if(button_release(GPIOB,BTN2_Pin,0) & TIM4->ARR >0){
-      htim4.Instance->ARR -= 1030;
-    }
-
+    HAL_Delay(25);
+    HAL_ADC_Start(&hadc1);
+    HAL_ADC_PollForConversion(&hadc1,100);
+    read = HAL_ADC_GetValue(&hadc1);
+    HAL_GPIO_WritePin(GPIOA,LED1_Pin,(read <= 800) ? 1 : 0); //800 1362 1925 2487 3050 3700
+    HAL_GPIO_WritePin(GPIOA,LED2_Pin,(read <= 1360) ? 1 : 0);
+    HAL_GPIO_WritePin(GPIOA,LED3_Pin,(read <= 1925) ? 1 : 0);
+    HAL_GPIO_WritePin(GPIOA,LED4_Pin,(read <= 2487) ? 1 : 0);
+    HAL_GPIO_WritePin(GPIOA,LED5_Pin,(read <= 3700 ) ? 1 : 0);
+    HAL_Delay(25);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -155,6 +158,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
@@ -254,16 +309,6 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef * htim){
-  counter--;
-  
-  HAL_GPIO_WritePin(GPIOA,LED1_Pin, (counter & 0x01) ? 1 : 0);
-  HAL_GPIO_WritePin(GPIOA,LED2_Pin, (counter & 0x02) ? 1 : 0);
-  HAL_GPIO_WritePin(GPIOA,LED3_Pin, (counter & 0x04) ? 1 : 0);
-  HAL_GPIO_WritePin(GPIOA,LED4_Pin, (counter & 0x08) ? 1 : 0);
-  HAL_GPIO_WritePin(GPIOA,LED5_Pin, (counter & 0x10) ? 1 : 0);
-  if(counter==0){
-    counter = 32;
-  }
 }
 
 /* USER CODE END 4 */
